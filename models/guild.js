19d57a88de9dc55cda39cd247668c1ffa0f532ad
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-06-14 18:29:17 +0200

    Update guild.js

--- models/guild.js
+++ models/guild.js
@@ -5,30 +5,28 @@ const guildSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   guildID: String,
   guildName: String,
-  prefix: {
-    "type": String,
-    "default": defaults.prefix
-  },
-  logChannel: {
-    "type": String,
-    "default": defaults.logChannel
-  },
-  welcomeMessage: {
-    "type": String,
-    "default": defaults.welcomeMessage
-  },
-  leaveMessage:{
-    "type": String,
-    "default": defaults.leaveMessage
-  },
-  welcomeChannel: {
-    "type": String,
-    "default": defaults.welcomeChannel
-  },
-  LOCALE: {
-    "type": String,
-    "default": defaults.LOCALE
-  }
+  prefix: { "type": String, "default": defaults.prefix },
+  registeredAt: { type: Number, default: Date.now() },
+  addons: { type: Object, default: {
+    welcome: {
+      enabled: false,
+      channel:  null,
+      message: null,
+      embed: false
+    },
+    goodbye: {
+      enabled: false,
+      channel:  null,
+      message: null,
+      embed: false
+    },
+    logs: { 
+      enabled: false,
+      channel:  null,
+      message: null,
+      embed: false
+    },
+  }}
 });
 
 module.exports = mongoose.model("Guild", guildSchema);
