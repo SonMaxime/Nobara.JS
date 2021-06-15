4bad49f57837c89dbd8a4eb839dda89379ba31d3
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-06-15 13:35:57 +0200

    optimise reactions time

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Collection } = require('discord.js');
 const { loadCommands, loadEvents } = require("./util/loader");
 const { Database } = require("bookman");
-const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });;
+const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'], restTimeOffset: 0 });;
 
 require("./util/functions")(client);
 client.config = require("./config");
