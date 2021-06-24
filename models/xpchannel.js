448b2e8f8a23a6bbb967eeb3c99ec251825af68e
SonMaxime <47661880+SonMaxime@users.noreply.github.com>
2021-06-24 23:01:48 +0200

    Delete xpchannel.js

--- models/xpchannel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const xpChannelSchema= new mongoose.Schema({
-    guildID: String,
-    channelID: String,
-});
-
-module.exports = mongoose.model('xpchannel', xpChannelSchema);
